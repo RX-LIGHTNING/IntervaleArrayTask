23c3a58b3ba627300d386194e8f748dc6f4a1dd3
Roman <mmamano@inbox.ru>
2022-12-27 10:55:59 +0300

    Recreated project with maven

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    /**
+     * Дан массив целых чисел. Нужно вернуть массив, в котором первым элементом
+     * будет количество положительных чисел, а во втором - сумма отрицательных
+     * 0 не является ни положительным ни отрицательным
+     * Если на вход прилетает пустой массив или null, то вернуть пустой массив
+     */
+    private static final int[] EMPTY_ARRAY = {};
+
+    public static int[] streams(int[] input) {
+        return Optional.ofNullable(input).map(arr -> {
+            if (arr.length != 0) {
+                int[] temporaryStorage = new int[2];
+                Arrays.stream(arr).forEach(
+                        num -> {
+                            if (num > 0) {
+                                temporaryStorage[0]++;
+                            } else {
+                                temporaryStorage[1] += num;
+                            }
+
+                        });
+                return temporaryStorage;
+            } else {
+                return null;
+            }
+        }).orElse(EMPTY_ARRAY);
+    }
+
+    public static int[] loops(int[] input) {
+        int[] result = new int[2];
+        if (input == null || input.length == 0)
+            return EMPTY_ARRAY;
+        for (int i : input) {
+            if (i > 0) {
+                result[0]++;
+            } else {
+                result[1] += i;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        System.out.println("stream test: " + Arrays.toString(streams(testArray)));
+        System.out.println("loop test: " + Arrays.toString(loops(testArray)));
+    }
+}
